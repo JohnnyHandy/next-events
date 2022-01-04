337f83b538536d76ba202e6543e043166c751b36
Roger Peixoto <duarterogerpeixoto@gmail.com>
2022-01-04 12:04:30 -0300

    feat: added semantic release generation when pushing to main

---
+++ release.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  branches: 'main',
+  repositoryUrl: 'https://github.com/JohnnyHandy/next-events',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ]
+}
