406ab2d12df6886daebddd8f32535b706591ff9a
Roger Peixoto <duarterogerpeixoto@gmail.com>
2022-01-04 06:02:32 -0300

    Adjusted script

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 ﻿#!/bin/bash
 
-echo "CI VARIABLE: $CI"
+echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ "$CI" == "1"]] ; then
+if [[ "$VERCEL_ENV" != "production" ]] ; then
   # Proceed with the build
-    echo "✅ - Build can proceed"
+  echo "✅ - Build can proceed"
   exit 1;
 
 else
