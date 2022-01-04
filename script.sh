0b7cc89ab2d08958dacf0a977fe5de45838a5454
Roger Peixoto <duarterogerpeixoto@gmail.com>
2022-01-04 05:17:52 -0300

    Added script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+echo "CI VARIABLE: $CI"
+
+if [[ "$CI" == "1"]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
